f6e16e8dcf78138761b03e4a8e0a87d4fae23dea
lj-sec <loganjack627@gmail.com>
2025-02-07 03:35:03 -0500

    Renamed, as well as added a progress bar to the emails and other QOL tweaks

--- Add-EmailsCSVtoDistributionList.ps1
+++ Add-EmailsCSVtoDistributionList.ps1
@@ -1,3 +1,18 @@
+#Requires -Version 5.1
+
+<#
+.SYNOPSIS
+A PowerShell script featuring a GUI to add emails into Microsoft Exchange Online distribution list groups.
+Requires PS 5.1.
+
+.NOTES
+Author: Logan Jackson
+Date: 2025
+
+.LINK
+Website: https://lj-sec.github.io/
+#>
+
 Add-Type -AssemblyName PresentationFramework, PresentationCore, System.Windows.Forms, System.Drawing
 
 # Welcome message
@@ -49,21 +64,21 @@ function New-Form()
     $script:form.Topmost = $true
 
     $script:label = New-Object System.Windows.Forms.Label
-    $script:label.Size = New-Object System.Drawing.Size(700,30)
+    $script:label.Size = New-Object System.Drawing.Size(700,35)
     $script:label.Location = New-Object System.Drawing.Point(25,25)
-    $script:label.Font = New-Object System.Drawing.Font("Arial", 12)
+    $script:label.Font = New-Object System.Drawing.Font("Arial", 11)
     $script:form.Controls.Add($script:label)
 
     $script:comboBox = New-Object System.Windows.Forms.ComboBox
     $script:comboBox.Size = New-Object System.Drawing.Size(675,30)
     $script:comboBox.Location = New-Object System.Drawing.Point(25,75)
-    $script:comboBox.Font = New-Object System.Drawing.Font("Arial", 12)
+    $script:comboBox.Font = New-Object System.Drawing.Font("Arial", 11)
     $script:comboBox.DropDownStyle = [System.Windows.Forms.ComboBoxStyle]::DropDownList
     $script:form.Controls.Add($script:comboBox)
     
     $okButton = New-Object System.Windows.Forms.Button
-    $okButton.Size = New-Object System.Drawing.Size(100,30)
-    $okButton.Font = New-Object System.Drawing.Font("Arial", 12)
+    $okButton.Size = New-Object System.Drawing.Size(150,35)
+    $okButton.Font = New-Object System.Drawing.Font("Arial", 11)
     $okButton.Location = New-Object System.Drawing.Point(25,250)
     $okButton.Text = 'OK'
     $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
@@ -71,9 +86,9 @@ function New-Form()
     $script:form.Controls.Add($okButton)
     
     $cancelButton = New-Object System.Windows.Forms.Button
-    $cancelButton.Size = New-Object System.Drawing.Size(100,30)
-    $cancelButton.Font = New-Object System.Drawing.Font("Arial", 12)
-    $cancelButton.Location = New-Object System.Drawing.Point(150,250)
+    $cancelButton.Size = New-Object System.Drawing.Size(150,35)
+    $cancelButton.Font = New-Object System.Drawing.Font("Arial", 11)
+    $cancelButton.Location = New-Object System.Drawing.Point(200,250)
     $cancelButton.Text = 'Cancel'
     $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
     $script:form.CancelButton = $cancelButton
@@ -87,15 +102,25 @@ try
 }
 catch
 {
-    $confirm = [System.Windows.MessageBox]::Show("ExchangeOnlineManagement Module not found, would you like to install now?`nThis is a requirement for script functionality.","Warning",[System.Windows.MessageBoxButton]::YesNo,[System.Windows.MessageBoxImage]::Warning)
-    if($confirm -eq "Yes")
+    try
     {
-        Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser -WarningAction 'SilentlyContinue' -Force
+        Get-PackageProvider -Name NuGet 1>$null -ErrorAction Stop
+    }
+    catch
+    {
+        [System.Windows.MessageBox]::Show("NuGet Package Manager is a requirement for script functionality, exiting script.","Error",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)
+        Exit 1
+    }
+    $confirm = [System.Windows.MessageBox]::Show("ExchangeOnlineManagement Module not found, would you like to install now?`nThis is a requirement for script functionality.","Warning",[System.Windows.MessageBoxButton]::YesNo,[System.Windows.MessageBoxImage]::Warning)
+    if($confirm -eq [System.Windows.MessageBoxResult]::Yes)
+    {
+        Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser -WarningAction 'SilentlyContinue' -Force -AllowClobber
         Import-Module -Name ExchangeOnlineManagement
     }
     else
     {
-        [System.Windows.MessageBox]::Show("ExchangeOnlineManagement Module is requirement for script functionality, exiting script.","Warning",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Information)   
+        [System.Windows.MessageBox]::Show("ExchangeOnlineManagement Module is a requirement for script functionality, exiting script.","Error",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)
+        Exit 1
     }
 }
 
@@ -106,25 +131,39 @@ try
 }
 catch
 {
-    [System.Windows.MessageBox]::Show("Could not connect to Exchange Online, exiting","Alert",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Warning) > $null
+    [System.Windows.MessageBox]::Show("Could not connect to Exchange Online, exiting","Error",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error) > $null
     Exit 1
 }
 
-[System.Windows.MessageBox]::Show("Connected, loading distribution lists. This may take a minute depending on the size of your organization.`nPress OK to continue","Alert",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Information) > $null
+[System.Windows.MessageBox]::Show("Connected. Press OK to load distribution lists. This may take a minute depending on the size of your organization.","Alert",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Information) > $null
+
+Write-Host "Loading (please allow some time)..."
 
 # Grab the user's email and distribution lists that they own
 $email = (Get-MailBox).Name
 $managedDLs = Get-DistributionGroup -WarningAction 'SilentlyContinue' | Where-Object {$_.ManagedBy -icontains "$email"}
 
+if ($managedDLs.Count -eq 0)
+{
+    [System.Windows.MessageBox]::Show("No managed distribution lists found. Exiting.","Error",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)
+    Exit 1
+}
+
 # Import the csv and and headers from it
 $emailsCsv = Import-Csv -LiteralPath $openFileDialog.FileName
 $headers = $emailsCsv[0].PSObject.Properties.Name
+
+if ($headers.Count -eq 0)
+{
+    [System.Windows.MessageBox]::Show("No headers in the CSV file found. Exiting.","Error",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)
+    Exit 1
+}
 
 # Prepare a form
 New-Form
 
 # Update explanation label
-$script:label.Text = "Select the column of the CSV containing email addresses to add:"
+$script:label.Text = "Select the column of the CSV containing emails:"
 
 # Populate the combobox with headers
 foreach($header in $headers)
@@ -145,11 +184,18 @@ else
     Exit 1
 }
 
+# Ensure that there was a header selected
+if($null -eq $selectedHeader)
+{
+    [System.Windows.MessageBox]::Show("No headers in the CSV file selected. Exiting.","Error",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)
+    Exit 1
+}
+
 # Prepare another form
 New-Form
 
 # Update explanation label
-$script:label.Text = "Select the distribution group you would like to add these emails to:"
+$script:label.Text = "Select the distribution group:"
 
 # Clear out and repopulate the combobox
 $script:comboBox.Items.Clear()
@@ -166,27 +212,45 @@ if ($result -eq [System.Windows.Forms.DialogResult]::OK)
 {
     $selectedDL = $script:comboBox.SelectedItem
 }
+else
+{
+    Exit 1
+}
+
+# Ensure there was a list selected
+if($null -eq $selectedDl)
+{
+    [System.Windows.MessageBox]::Show("No distribution group selected. Exiting.","Error",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)
+    Exit 1
+}
 
 # Prepare a warning message for if emails fail to be added
 $warningMessage = "The following emails could not be added (check for typos or if they are already on the list):`n"
 
 # Assume warning message will not show
 $warning = $false
+$numAdded = 0
+$index = 0
 
 # Loop to add emails to list
 foreach($email in $emailsCsv)
 {
+    $index++
+    $percent = [Math]::Round(($index/$emailsCsv.Length)*100, 2)
+    Write-Progress -Activity "Adding $member..." -Status "$percent% Complete:" -PercentComplete $percent
+
     # Grab the email
     $member = $email."$($selectedHeader)"
     # Attempt to add, if it fails add the user to the warning message
     try
     {
         Add-DistributionGroupMember -Identity $selectedDL -Member $member -ErrorAction Stop
+        $numAdded++
     }
     catch
     {
         $warningMessage += "$($email."$($selectedHeader)")`n"
-        # Update the warning messaage bool
+        # Update the warning messaage boolean
         $warning = $true
     }
 }
@@ -196,9 +260,9 @@ Disconnect-ExchangeOnline -Confirm:$false
 # Check for warning message, if not give success message
 if($warning)
 {
-    [System.Windows.MessageBox]::Show("$warningMessage","Alert",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Warning) > $null
+    [System.Windows.MessageBox]::Show("$warningMessage`n$numAdded emails were successfully added.","Alert",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Warning) > $null
 }
 else
 {
-    [System.Windows.MessageBox]::Show("Success!","Alert",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Information) > $null
+    [System.Windows.MessageBox]::Show("Success! All $numAdded emails added to list","Alert",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Information) > $null
 }
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Convert-CSVtoDL
-A PowerShell script featuring a GUI to add emails into Microsoft Exchange Online distribution groups.
+# Add-EmailsCSVtoDistributionList
+Made for those that need to add a bulk amount of emails to a distribution list group quickly from their responses on a Google Form or similar, as opposed to–annoyingly–manually inputting every email address into Exchange.
+
+## Disclaimer:
+The -AllowClobber parameter on the installation of the ExchangeOnlineManagement module is present to ensure its functionality; make sure you understand the implications of this and it doesn’t overwrite any PowerShell cmdlets you care about. I recommend installing the module manually if this is of any concern to you.
+The author is not responsible for any misuse, malpractice, or damage that may stem from the use of this script. Please review the script and upload the file to https://www.virustotal.com/gui/home/upload if there are any concerns. This script does not require admin rights. RUN AT OWN RISK.
+
+## Requirements:
+To be a manager of the distribution group that you wish to add these emails to.
+PowerShell version 5.1 or later (this is typically pre-installed on latest Windows versions).
+NuGet Package Provider (the script will install this if it detects it as missing).
+Exchange Online Management PowerShell module (the script will install this with -AllowClobber if it detects it missing).
+
+## Steps:
+Acquire your .csv containing a column full of emails, preferably with a header (first row) that allows you to note in the future that it is the correct column. If coming from Sheets or Excel, in the spreadsheet that you wish to add the emails from, click “File” and download the spreadsheet as a Comma Separated Values (.csv) file.
+
+Download the script. Open File Explorer, navigate to the script in your Downloads folder, right click, and click “Run with PowerShell.”
+
+Follow the instructions in the script. There may be periods of waiting for the script to process or download if applicable. If prompted to install NuGet or ExchangeOnline, please do both. You will be prompted to sign in to your Microsoft account that manages the distribution group you wish to add these emails to.
